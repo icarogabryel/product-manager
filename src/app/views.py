51e5d5a77788866c7b1ee107eb065a42aa2a5b68
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-01-26 01:21:53 -0300

    add view for Product

--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
+from django.views.generic import ListView
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product-list.html'
+    context_object_name = 'products'
--- src/core/urls.py
+++ src/core/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
+from app.views import ProductListView
 
 
 urlpatterns = [
         path('admin/', admin.site.urls),
+        path('', ProductListView.as_view(), name='product-list'),
 ]
